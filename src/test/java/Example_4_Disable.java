13b6483a55592f564c58ea3a913c51fde6175bb5
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-05-25 15:02:50 +0300

    Feature

--- src/test/java/Example_4_Disable.java
+++ src/test/java/Example_4_Disable.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 
 public class Example_4_Disable {
 
-    @Disabled("Disabled until bug #99 has been fixed")
-    class DisabledClassDemo {
+    @Disabled("Disabled until bug #42 has been resolved")
+    @Test
+    void testWillBeSkipped() {
+    }
 
-        @Test
-        void testWillBeSkipped() {
-        }
-
+    @Test
+    void testWillBeExecuted() {
     }
 }
